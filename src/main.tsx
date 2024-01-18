cdb5a9b99a9ed0f55145db7602f528df6739fa24
daniel <d.danielprince2003@gmail.com>
2024-01-18 18:35:08 +0530

    feat:debugging finished

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import SecondApp from './secondApp.tsx'
 import 'bootstrap/dist/css/bootstrap.css'
 import { BrowserRouter } from 'react-router-dom'
 import Routing from './Routing.tsx'
