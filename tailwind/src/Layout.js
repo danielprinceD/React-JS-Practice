2aed43c4fb0de723e0d386f5d11796714bb20a59
daniel <d.danielprince2003@gmail.com>
2024-07-06 16:38:12 +0530

    feat:columns and float in container

--- tailwind/src/Layout.js
+++ tailwind/src/Layout.js
@@ -9,6 +9,30 @@ const Layout = () => {
         <div className="container mx-auto text-white rounded-md p-2 text-center bg-red-400">
           This is Container
         </div>
+        <div className="container mx-auto leading-7 columns-2  text-justify">
+          <img
+            className="float-end m-3 rounded-md shadow-md shadow-slate-700"
+            src="https://picsum.photos/200/100"
+          />
+          <div>
+            1 . Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error
+            eligendi dolor animi tenetur iure adipisci illum dolores amet.
+            Repellendus accusamus unde, eligendi omnis est iusto recusandae
+            dicta architecto pariatur ea.
+          </div>
+          <div>
+            2 . Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error
+            eligendi dolor animi tenetur iure adipisci illum dolores amet.
+            Repellendus accusamus unde, eligendi omnis est iusto recusandae
+            dicta architecto pariatur ea.
+          </div>
+          <div>
+            3. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error
+            eligendi dolor animi tenetur iure adipisci illum dolores amet.
+            Repellendus accusamus unde, eligendi omnis est iusto recusandae
+            dicta architecto pariatur ea.
+          </div>
+        </div>
       </div>
     </>
   );
