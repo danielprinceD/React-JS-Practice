9b3134c9d1833e6583a4be930280fadd7a36b7f9
daniel <d.danielprince2003@gmail.com>
2024-07-06 16:10:48 +0530

    feat:layout added

---
+++ tailwind/src/Layout.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Layout = () => {
+  return (
+    <>
+      <div className="h-screen w-screen bg-red-50">
+        <h3 className="text-2xl md:text-5xl text-center">Layouts</h3>
+
+        <div className="container mx-auto text-white rounded-md p-2 text-center bg-red-400">
+          This is Container
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
