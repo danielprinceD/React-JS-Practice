e77ff2ed76e635a6f8840b9aa785ea29ae8d7bda
daniel <d.danielprince2003@gmail.com>
2024-07-06 20:10:47 +0530

    feat:completed border

---
+++ tailwind/src/Borders.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Borders = () => {
+  return (
+    <div className="h-screen bg-red-200 rounded-md m-2">
+      <div className="container text-center  mx-auto bg-red-500 text-white">
+        Borders
+      </div>
+      <div className="container mx-auto border-4 border-blue-700">Border</div>
+      <input
+        type="text"
+        className="focus:outline-rose-500 outline outline-1 outline-purple-700"
+      />
+      <button className="bg-red-500 ring ring-offset-2 text-white px-4 py-1 rounded-md active:bg-red-400">
+        Button
+      </button>
+      <div className="container m-4">
+        <div
+          className="bg-white p-3 justify-center rounded-lg shadow-md
+        shadow-black flex flex-col gap-3 w-32 h-32"
+        >
+          <div className="text-center">Hello</div>
+          <div className="bg-red-500  text-white py-1 rounded-md active:bg-red-400 text-center">
+            Button
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Borders;
--- tailwind/src/index.js
+++ tailwind/src/index.js
@@ -4,10 +4,12 @@ import "./index.css";
 import App from "./App";
 import MyGrid from "./MyGrid";
 import Layout from "./Layout";
+import Borders from "./Borders";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
+    <Borders />
     <Layout />
     <App />
     <MyGrid />
